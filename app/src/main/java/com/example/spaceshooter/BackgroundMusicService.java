a77d2f88a9e5f9d961d039d027977f909fd856b8
DucNguyen1311 <duyduc.miaham@gmail.com>
2024-03-23 23:49:16 +0700

    Added some stuff

---
+++ app/src/main/java/com/example/spaceshooter/BackgroundMusicService.java
@@ -0,0 +1,43 @@
+package com.example.spaceshooter;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+
+import java.security.Provider;
+
+public class BackgroundMusicService extends Service {
+
+    MediaPlayer player;
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        player = MediaPlayer.create(this, R.raw.background);
+        player.setLooping(true);
+        player.setVolume(80,80);
+        player.start();
+    }
+
+    @Override
+    public void onStart(Intent intent, int startId) {
+        super.onStart(intent, startId);
+    }
+
+    public void onDestroy() {
+        // TODO Auto-generated method stub
+        super.onDestroy();
+    }
+
+    protected void onNewIntent() {
+        player.pause();
+    }
+}
---
+++ app/src/main/java/com/example/spaceshooter/LowBatteryNotification.java
@@ -0,0 +1,27 @@
+package com.example.spaceshooter;
+
+import android.app.Service;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+
+import java.security.Provider;
+
+public class LowBatteryNotification extends Service {
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(Intent.ACTION_BATTERY_LOW);
+        LowBatteryBroadcastReceiver lowBatteryBroadcastReceiver= new LowBatteryBroadcastReceiver();
+        registerReceiver(lowBatteryBroadcastReceiver, intentFilter);
+    }
+}
